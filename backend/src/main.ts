55e53ba3bb494beb9b371cd1ec69c80c587c8df0
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-02 14:00:33 -0300

    Adicionado suporte ao Swagger seguindo as orientações da documentação do Nest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Survey Manager')
+    .setDescription(
+      'Aplicação para o gerenciamento e armazenamento de pesquisas',
+    )
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('http://localhost:3000/', 'Production')
+    .addTag('SurveyManager')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
