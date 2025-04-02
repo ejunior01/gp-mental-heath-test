ad55c5eba611575ebe80638bb2f00e6f274c6530
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-02 19:04:52 -0300

    Adicionado validações ao DTO de criação de Survey e ajustado o DTO de atualização para herdar diretamente de CreateSurveyDto

--- backend/src/modules/survey/dto/create-survey.dto.ts
+++ backend/src/modules/survey/dto/create-survey.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSurveyDto {}
+import { IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateSurveyDto {
+  @IsNotEmpty()
+  @MinLength(2)
+  code: string;
+
+  @IsNotEmpty()
+  noteOne: number;
+
+  @IsNotEmpty()
+  noteTwo: number;
+}
