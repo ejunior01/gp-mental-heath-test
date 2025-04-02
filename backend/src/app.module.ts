caf33162ad77cc78fc831525b2c780dd2a4679d7
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-02 14:38:07 -0300

    Adicionado o Prisma para manipulação do banco de dados, e configurado os modelos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { SurveyModule } from './modules/survey/survey.module';
+import { PrismaService } from './shared/prisma/prisma.service';
 
 @Module({
-  imports: [SurveyModule],
+  imports: [SurveyModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
