b88ca0c120f051d32f28109fec7c9ef06526ab9a
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-02 23:38:51 -0300

    adicionado suporte a CORS e atualizado portas para ambiente local e produção

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
     .setTitle('Survey Manager')
@@ -13,13 +16,13 @@ async function bootstrap() {
       'Aplicação para o gerenciamento e armazenamento de pesquisas',
     )
     .setVersion('1.0')
-    .addServer('http://localhost:3000/', 'Local environment')
-    .addServer('http://localhost:3000/', 'Production')
+    .addServer('http://localhost:3001/', 'Local environment')
+    .addServer('http://localhost:3001/', 'Production')
     .addTag('SurveyManager')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
