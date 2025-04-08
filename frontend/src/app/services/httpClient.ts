c94d2e4dc33b6a8fe9fa3810fce12055a050bc16
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-08 10:36:38 -0300

    Adicionado endpoints da API para pesquisas e upload de pesquisas no httpClient

--- frontend/src/app/services/httpClient.ts
+++ frontend/src/app/services/httpClient.ts
@@ -3,3 +3,8 @@ import axios from "axios";
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+export const ApiEndpoints = {
+  surveyApiEndpoint: "survey",
+  uploadSurveyApiEndpoint: "survey-upload",
+};
