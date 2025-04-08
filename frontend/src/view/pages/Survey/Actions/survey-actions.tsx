f8fcf4cc46c2c437f7b2db2d2225227bb02ce8b3
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-07 23:57:37 -0300

    Alinhamento das actions ao meio

--- frontend/src/view/pages/Survey/Actions/survey-actions.tsx
+++ frontend/src/view/pages/Survey/Actions/survey-actions.tsx
@@ -1,12 +1,11 @@
 import { DeleteSurveyDialog } from "@/view/pages/Survey/Actions/DeleteSurvey/delete-survey-dialog";
 import { UpdateSurveyDialog } from "@/view/pages/Survey/Actions/UpdateSurvey/update-survey-dialog";
 
-
 export function SurveyActions({ code }: { code: string }) {
-    return (
-        <div>
-            <UpdateSurveyDialog code={code} />
-            <DeleteSurveyDialog code={code} />
-        </div>
-    )
+  return (
+    <div className="w-full flex items-center justify-center">
+      <UpdateSurveyDialog code={code} />
+      <DeleteSurveyDialog code={code} />
+    </div>
+  );
 }
